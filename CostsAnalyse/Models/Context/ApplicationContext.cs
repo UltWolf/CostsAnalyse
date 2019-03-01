9a35e33ef413afd06dacd26d78b5e0794e0d78dc
UltWolf <ultwolf@gmail.com>
2019-03-01 15:41:01 +0200

    Add Context to Data

---
+++ CostsAnalyse/Models/Context/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostsAnalyse.Models.Context
+{
+    public class ApplicationContext:DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> dbContext) : base(dbContext)
+        {
+
+        }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Information> Informations { get; set; }
+        public DbSet<Price> Prices { get; set; }
+    }
+}
--- CostsAnalyse/Startup.cs
+++ CostsAnalyse/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CostsAnalyse.Models.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -30,7 +32,7 @@ namespace CostsAnalyse
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
+            services.AddDbContext<ApplicationContext>(m=> m.UseSqlServer(Configuration.GetConnectionString("StringConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
