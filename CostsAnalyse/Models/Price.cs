26a826b87f4c9279c043296ed55daaae91908c77
UltWolf <ultwolf@gmail.com>
2019-03-01 14:31:59 +0200

    Add entities to websites

---
+++ CostsAnalyse/Models/Price.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostsAnalyse.Models
+{
+    public class Price
+    {
+        public int Id { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ CostsAnalyse/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostsAnalyse.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public Information Information { get; set; }
+        public Price Price { get; set; }
+        public Company Company { get; set; }
+        
+    }
+}
