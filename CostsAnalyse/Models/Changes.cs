a7ea70cced570ec834d2e7a36e294eda0b82557e
UltWolf <ultwolf@gmail.com>
2019-03-08 16:35:05 +0200

    Add class Changes

--- CostsAnalyse/Models/Changes.cs
+++ CostsAnalyse/Models/Changes.cs
@@ -4,8 +4,11 @@ namespace CostsAnalyse.Models
 {
     public class Changes
     {
+        public int Id { get; set; }
+        public UserStatus Statuses { get; set; }
         public int UserId{get;set;}
         public DateTime Date { get; set; }
+        public Data.TypeOfChange change {get;set;}
         public int IdGoal { get; set; }         
     }
 }
