8f5d85af713474d340df6b23b57cd8a3afd2c71c
UltWolf <ultwolf@gmail.com>
2019-06-15 14:12:07 +0300

    Update RozetkaPageDriver , add initializing pagination

--- CostsAnalyse/Services/PageDrivers/RozetkaPageDriver.cs
+++ CostsAnalyse/Services/PageDrivers/RozetkaPageDriver.cs
@@ -15,17 +15,19 @@ using CostsAnalyse.Models.Context;
 
 namespace CostsAnalyse.Services.PageDrivers
 {
-    public class RozetkaPageDriver:IPageDrivers
+    public class RozetkaPageDriver : IPageDrivers
     {
         List<string> proxyList = new List<string>();
         private readonly ApplicationContext _context;
-        public RozetkaPageDriver(ApplicationContext Context) {
+        public RozetkaPageDriver(ApplicationContext Context)
+        {
             this.proxyList = ProxyServerConnectionManagment.GetProxyHrefs();
             this._context = Context;
             GenerateHrefs();
         }
-        public HashSet<String> GetPages() {
-            HashSet<String> pages ;
+        public HashSet<String> GetPages()
+        {
+            HashSet<String> pages;
             using (FileStream fs = new FileStream("RozetkaHrefs.txt", FileMode.Open, FileAccess.Read))
             {
                 BinaryFormatter bf = new BinaryFormatter();
@@ -33,34 +35,39 @@ namespace CostsAnalyse.Services.PageDrivers
             }
             return pages;
         }
-        public void GetProducts() {
-            
+        public void GetProducts()
+        {
+
             var pages = GetPages();
             List<Product> products = new List<Product>();
             foreach (var page in pages)
             {
                 if (proxyList.Count <= 1)
                 {
-                        break;
+                    break;
                 }
-               ParseProductsFromPage(page,0);
+                ParseProductsFromPage(page, 0);
             }
-           
+
         }
-        public void ParseProductsFromPage(string url,int index)
+        public void ParseProductsFromPage(string url, int index)
         {
-            
+            string baseUrl = url;
+            if (index != 0)
+            {
+                url += "/page=" + index + "/";
+            }
             ThreadDelay.Delay();
-           
+
             try
             {
                 WebRequest WR = WebRequest.Create(url);
                 WR.Method = "GET";
                 string[] fulladress = proxyList[++index].Split(":");
-                var (adress, port) = (fulladress[0],int.Parse(fulladress[1]));
+                var (adress, port) = (fulladress[0], int.Parse(fulladress[1]));
                 WebProxy myproxy = new WebProxy(adress, port);
                 myproxy.BypassProxyOnLocal = false;
-                WR.Proxy = myproxy ;
+                WR.Proxy = myproxy;
                 WebResponse response = WR.GetResponse();
                 string html;
                 using (Stream stream = response.GetResponseStream())
@@ -70,74 +77,90 @@ namespace CostsAnalyse.Services.PageDrivers
                         html = reader.ReadToEnd();
                     }
                 }
-                
+
                 HtmlParser parser = new HtmlParser();
                 var parseElement = parser.ParseDocument(html);
                 var divsWithProduct = parseElement.GetElementsByClassName("g-i-tile-catalog");
+                
                 RozetkaParser rp = new RozetkaParser();
                 foreach (var div in divsWithProduct)
                 {
                     try
                     {
                         string urlForPage = div.GetElementsByClassName("g-i-tile-i-title")[0].GetElementsByTagName("a")[0].GetAttribute("href");
-                        var product = rp.GetProduct(urlForPage,ref proxyList);
-                        if(!product.IsNull()){
-                        var productFromContext = _context.Products.FirstOrDefault(m=> m.Index == product.Index);
-                        var currentCost = product.LastPrice[0].Cost;
-                        if(productFromContext ==null){
-                            product.Price = product.LastPrice;
-                            product.Min = currentCost;
-                            product.Max = currentCost;
-                            _context.Add(product);
-                        }else{
-                            productFromContext.Price.Add(product.LastPrice);
-                            var  lastPrice = productFromContext.LastPrice.FirstOrDefault(m=>m.Company.Equals(product.LastPrice[0].Company));
-                            if(lastPrice!=null){
-                                lastPrice = product.LastPrice[0];
-                            }else{
-                                productFromContext.LastPrice.Add(product.LastPrice[0]);
+                        var product = rp.GetProduct(urlForPage, ref proxyList);
+                        if (!product.IsNull())
+                        {
+                            var productFromContext = _context.Products.FirstOrDefault(m => m.Index == product.Index);
+                            var currentCost = product.LastPrice[0].Cost;
+                            if (productFromContext == null)
+                            {
+                                product.Price = product.LastPrice;
+                                product.Min = currentCost;
+                                product.Max = currentCost;
+                                _context.Add(product);
                             }
-                            if(currentCost>productFromContext.Max){
-                                productFromContext.Max = currentCost;
-                            }else if(currentCost< productFromContext.Min){
-                                productFromContext.Min = currentCost;
+                            else
+                            {
+                                productFromContext.Price.Add(product.LastPrice);
+                                var lastPrice = productFromContext.LastPrice.FirstOrDefault(m => m.Company.Equals(product.LastPrice[0].Company));
+                                if (lastPrice != null)
+                                {
+                                    lastPrice = product.LastPrice[0];
+                                }
+                                else
+                                {
+                                    productFromContext.LastPrice.Add(product.LastPrice[0]);
+                                }
+                                if (currentCost > productFromContext.Max)
+                                {
+                                    productFromContext.Max = currentCost;
+                                }
+                                else if (currentCost < productFromContext.Min)
+                                {
+                                    productFromContext.Min = currentCost;
+                                }
+                                _context.Products.Update(productFromContext);
                             }
-                            _context.Products.Update(productFromContext);
-                        }
-                        
+
                             _context.SaveChanges();
                         }
-                        
+
                     }
                     catch (Exception ex)
-                    { 
+                    {
                     }
                 }
-            }
-            catch (Exception ex) when (ex.Message == "Подключение не установлено, т.к.конечный компьютер отверг запрос на подключение Подключение не установлено, т.к.конечный компьютер отверг запрос на подключение")
-            {
-
-                if (proxyList.Count-1> index)
+                var paginator = parseElement.GetElementsByClassName("paginator-catalog");
+                string page = "";
+                if (paginator.Length > 0)
                 {
-                    proxyList.Remove(proxyList[index]);
+                    var lis = paginator[0].GetElementsByTagName("li");
+                    for (int i = 0; i < lis.Length; i++)
+                    {
+                        if (lis[i].ClassList.Contains("active"))
+                        {
+                            page = lis[++i].GetElementsByTagName("span")[0].TextContent;
+                            break;
+                        }
+                    }
                 }
-                ParseProductsFromPage(url,index);
+                if (page != "")
+                {
+                    ParseProductsFromPage(baseUrl, int.Parse(page));
+                }
             }
+            
             catch (Exception ex)
             {
-
-                if (index < proxyList.Count - 1)
-                {
-                    index++;
-                    ParseProductsFromPage(url,index++);
-                }
-            }
-            ProxyServerConnectionManagment.SerializeByPuts(proxyList);
+ 
+            } 
         }
 
         public void GenerateHrefs()
         {
-            if (!File.Exists("RozetkaHrefs.txt")) {
+            if (!File.Exists("RozetkaHrefs.txt"))
+            {
                 RozetkaMenuDriver RMD = new RozetkaMenuDriver();
                 RMD.GetPagesAuto();
             }
---
+++ TestsForCostsAnalyse/Tests/ParsersTests/RozetkaTests/RozetkaPageDriverTests.cs
@@ -0,0 +1,19 @@
+﻿using CostsAnalyse.Models.Context;
+using CostsAnalyse.Services.PageDrivers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TestsForCostsAnalyse.Tests.ParsersTests.RozetkaTests
+{
+    public class RozetkaPageDriverTests
+    {
+        [Fact]
+        public void GetListWithoutExceptions()
+        {
+            RozetkaPageDriver RPD = new RozetkaPageDriver(new ApplicationContext());
+            RPD.ParseProductsFromPage("https://rozetka.com.ua/notebooks/c80004/filter", 0);
+        }
+    }
+}
